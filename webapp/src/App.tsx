aa48b299cdc267fe876e4b9f3610d99eac8d6c93
pablo <pablo@adfinem.net>
2023-12-30 12:02:57 +0100

    Revert "introduce image tiles navigation menu on main screen"
    This reverts commit 29eea9dced01fe1a495ea880549f85e0eb3933a0.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,22 +7,17 @@ import './App.css';
 import { renderSugar } from "./pages/sugar";
 import { renderPressure } from "./pages/pressure";
 import { renderWelcome } from "./pages/welcome";
-import { renderCholesterol } from "./pages/cholesterol";
-import { renderUrineAcid } from "./pages/urine_acid";
 import { renderBloodNavBar } from "./components/BloodNavBar";
-import { renderCharts } from "./pages/charts";
 
 export const App = () => {
   return (
     <div>
       { renderBloodNavBar() }  
       <Routes>
-        <Route path="/" element={renderWelcome()}></Route>
         <Route path="/pressure" element={renderPressure()}></Route>
         <Route path="/sugar" element={renderSugar()}></Route>
-        <Route path="/urine_acid" element={renderUrineAcid()}></Route>
-        <Route path="/cholesterol" element={renderCholesterol()}></Route>
-        <Route path="/charts" element={renderCharts()}></Route>
+        <Route path="/welcome" element={renderWelcome()}></Route>
+        <Route path="/" element={renderWelcome()}></Route>
       </Routes>  
     </div>
   )
--- webapp/src/pages/welcome.tsx
+++ webapp/src/pages/welcome.tsx
@@ -3,44 +3,23 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
+// import Toolbar from "@mui/material/Toolbar";
+
+// import ImageListItem from "@material-ui/core/ImageListItem";
+
+
 import i18n from "i18next";
 import { useTranslation } from "react-i18next";
 import Grid from "@mui/material/Grid";
+import Item from "@mui/material/ListItem";
 import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export const renderWelcome = () => {
     const { t } = useTranslation('translation')
     
     console.log("Welcome render")
-
-    const navigate = useNavigate();
-
-    const handlePressureClick = () => {
-        console.log('handlePressureClick');
-        navigate('/pressure');
-    }
-
-    const handleSugarClick = () => {
-        console.log('handleSugarClick');
-        navigate('/sugar');
-    }
-
-    const handleUrineAcidClick = () => {
-        console.log('handleUrineAcidClick');
-        navigate('/urine_acid');
-    }
-
-    const handleCholesterolClick = () => {
-        console.log('handleUrineAcidClick');
-        navigate('/cholesterol');
-    }
-
-    const handleGraphsClick = () => {
-        console.log('handleGraphsClick');
-    }
 
     i18n.addResource('gb', 'translation', 'welcome', 'Blood Parameters Storage System (BPS2)');
     i18n.addResource('de', 'translation', 'welcome', 'BPS2');
@@ -50,93 +29,44 @@ export const renderWelcome = () => {
     i18n.addResource('de', 'translation', 'pressure', 'Druck');
     i18n.addResource('pl', 'translation', 'pressure', 'Ciśnienie');
 
-    i18n.addResource('gb', 'translation', 'sugar', 'Sugar');
-    i18n.addResource('de', 'translation', 'sugar', 'Zucker');
-    i18n.addResource('pl', 'translation', 'sugar', 'Cukier');
-
-    i18n.addResource('gb', 'translation', 'urine acid', 'Urine acid');
-    i18n.addResource('de', 'translation', 'urine acid', 'Harnsäure');
-    i18n.addResource('pl', 'translation', 'urine acid', 'Kwas moczowy');
-
-    i18n.addResource('gb', 'translation', 'cholesterol', 'Cholesterol');
-    i18n.addResource('de', 'translation', 'cholesterol', 'Cholesterol');
-    i18n.addResource('pl', 'translation', 'cholesterol', 'Cholesterol');
-
-    i18n.addResource('gb', 'translation', 'charts', 'Charts');
-    i18n.addResource('de', 'translation', 'charts', 'Diagramme');
-    i18n.addResource('pl', 'translation', 'charts', 'Wykresy');
-
     return (
         <AppBar position="static">
         <Container maxWidth="lg">
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <Button variant="text" size="small"
-                        sx={{":hover": {
-                                bgcolor: "#AF5",
-                                color: "white"
-                                }}}
-                        style={{display: "flex", flexDirection: "column", textTransform: "none"}}
-                        onClick={handlePressureClick}>
-                        <img src="logo512.png" width="100" alt="folder"
-                            // onMouseOver={handleMouseOver}
-                            />
-                        <label>{t('pressure')}</label>
-                    </Button>
-                </Grid>
-                <Grid item xs={4}>
-                    <Button variant="text" size="small"
-                        sx={{":hover": {
-                                bgcolor: "#AF5",
-                                color: "white"
-                                }}}
-                                style={{display: "flex", flexDirection: "column", textTransform: "none"}}
-                        onClick={handleSugarClick}>
-                        <img src="logo512.png" width="100" alt="folder"/>
-                        <label>{t('sugar')}</label>
-                    </Button>
-                </Grid>
-                <Grid item xs={4}>
-                    <Button variant="text" size="small"
-                    sx={{":hover": {
-                                bgcolor: "#AF5",
-                                color: "white"
-                                }}}
-                                style={{display: "flex", flexDirection: "column", textTransform: "none"}}
-                        onClick={handleUrineAcidClick}>
-                        <img src="logo512.png" width="100" alt="folder"/>
-                        <label>{t('urine acid')}</label>
-                    </Button>
-                </Grid>
-                <Grid item xs={4}>
-                    <Button variant="text" size="small"
-                        sx={{":hover": {
-                                bgcolor: "#AF5",
-                                color: "white"
-                                }}}
-                                style={{display: "flex", flexDirection: "column", textTransform: "none"}}
-                        onClick={handleCholesterolClick}>
-                        <img src="logo512.png" width="100" alt="folder"/>
-                        <label>{t('cholesterol')}</label>
-                    </Button>
-                </Grid>
-                <Grid item xs={4}>
-                    <Button variant="text" size="small"
-                        sx={{":hover": {
-                                bgcolor: "#AF5",
-                                color: "white"
-                                }}}
-                                style={{display: "flex", flexDirection: "column", textTransform: "none"}}
-                        onClick={handleGraphsClick}
-                        // onMouseOver={handleMouseOver}
-                        >
-                        <img src="logo512.png" width="100" alt="folder"/>
-                        <label>{t('charts')}</label>
-                    </Button>
-                </Grid>
-            </Grid>
+        <Grid container spacing={2}>
+  <Grid item xs={8}>
+    <Item>
+        <Link to="/pressure">
+        <Button variant="outlined">
+            {t("pressure")}
+        </Button>
+        </Link>
+        xs=8</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>
+    <Link to="/sugar">
+        <Button variant="outlined">
+            {t("sugar")}
+        </Button>
+        </Link>
+        xs=4</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+</Grid>
+            {/*<Toolbar> */ }
+                <Typography sx={{flexGrow: 1, fontWeight: 700}}>
+                    {t('welcome')}
+                    {/* <Link style={{ textDecoration: "none", color: "white" }} to="/pressure">{t("pressure")}</Link> */}
+            
+                </Typography>
+            {/* </Toolbar> */}
         </Container>
-    </AppBar>
+        </AppBar>
     )
 }
 
