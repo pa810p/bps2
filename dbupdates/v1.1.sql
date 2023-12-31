25de2c8a837549502414213d2c615fbe25b33391
pablo <pablo@adfinem.net>
2023-12-31 21:47:16 +0100

    add missing sequence creation

--- dbupdates/v1.1.sql
+++ dbupdates/v1.1.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS urine_acid;
 DROP SEQUENCE IF EXISTS urine_acid_id_seq;
 
+CREATE SEQUENCE urine_acid_id_seq start 1 increment 1 minvalue 1 cache 1;
+
 CREATE TABLE IF NOT EXISTS urine_acid (
   id int4 DEFAULT nextval('urine_acid_id_seq') NOT NULL,
   datetime TIMESTAMP NOT UNLL UNIQUE,
