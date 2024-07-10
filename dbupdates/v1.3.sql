3463ca12b8de607d673e08ea99746b9d81f79472
pa810p <pa810p@gmail.com>
2024-07-10 23:16:31 +0200

    46-add-constants-norms-for-referal - feature and tests

--- dbupdates/v1.3.sql
+++ dbupdates/v1.3.sql
@@ -1,35 +1,18 @@
-CREATE TABLE IF NOT EXISTS pressure (
-	datetime TIMESTAMP UNIQUE,
-	systolic INTEGER, 
-	diastolic INTEGER, 
-	pulse INTEGER, 
-	comment TEXT DEFAULT ''
-);
+DROP TABLE IF EXISTS norms;
+DROP SEQUENCE IF EXISTS norms_id_seq;
 
-CREATE TABLE IF NOT EXISTS sugar (
-  datetime TIMESTAMP UNIQUE,
-  sugar INTEGER NOT NULL,
-  comment TEXT DEFAULT ''
-);
+DROP INDEX IF EXISTS norms_name_idx;
 
-CREATE TABLE IF NOT EXISTS urine_acid (
-  datetime TIMESTAMP UNIQUE,
-  urine INTEGER NOT NULL,
-  comment TEXT DEFAULT ''
-);
-
-CREATE TABLE IF NOT EXISTS cholesterol (
-  datetime TIMESTAMP UNIQUE,
-  cholesterol INTEGER NOT NULL,
-  comment TEXT DEFAULT ''
-);
+CREATE SEQUENCE norms_id_seq start 1 increment 1 minvalue 1 cache 1;
+CREATE UNIQUE index norms_name_idx ON norms (name, human);
 
 CREATE TABLE IF NOT EXISTS norms (
+  id int4 DEFAULT nextval('norms_id_seq') NOT NULL,
   name TEXT,
   human TEXT,
   vmin INTEGER,
   vmax INTEGER
- );
+);
 
 INSERT INTO norms (name, human, vmin, vmax) VALUES ('systolic', 'm', 100, 140);
 INSERT INTO norms (name, human, vmin, vmax) VALUES ('systolic', 'f', 100, 140);
@@ -45,12 +28,13 @@ INSERT INTO norms (name, human, vmin, vmax) VALUES ('urine acid', 'm', 300, 360)
 INSERT INTO norms (name, human, vmin, vmax) VALUES ('urine acid', 'f', 240, 300);
 INSERT INTO norms (name, human, vmin, vmax) VALUES ('urine_acid', 'c', 210, 240);
 
-INSERT INTO norms (name, human, vmin, vmax) VALUES ('cholesterol', 'm', 50, 518);
+INSERT INTO norms (name, human, vmin, vmax) VALUES ('cholesterol', 'm', 0, 518);
 
 CREATE UNIQUE index norms_name_idx ON norms (name, human);
 
 ALTER TABLE sugar ADD COLUMN stomach TEXT;
+UPDATE sugar SET stomach='f' WHERE to_char(datetime, 'HH24:MI') > '10:00';
+UPDATE sugar SET stomach='e' WHERE to_char(datetime, 'HH24:MI') < '10:00';
+
+DROP INDEX IF EXISTS sugar_stomach_idx;
 CREATE INDEX sugar_stomach_idx ON sugar(stomach);
-
-
-
